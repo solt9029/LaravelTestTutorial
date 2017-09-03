cad28e143f630713e6b28b7af085971c69d2ba6d
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-03 21:08:28 +0900

    UsersController@jsonを追加しました

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,17 @@ class UsersController extends Controller
     {
         return 'test';
     }
+
+    public function json()
+    {
+        $array = [
+            'test' => 'test',
+            'comment' => 'okay',
+            'age' => 20,
+            'version' => 5.4,
+            'friends' => ['John', 'Amy', 'Kath']
+        ];
+
+        return $array;
+    }
 }
