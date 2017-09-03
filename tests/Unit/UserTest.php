541267789db77b13ed880091138d4aabdd3932b4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-09-03 20:30:22 +0900

    UserTestをつくりました

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testTest()
+    {
+        $response = $this->get('/users/test');
+        
+        $response->assertStatus(200);
+    }
+}
